81b47e2c66816640f96fb944556ccff6c8f516fb
Alan Guerra <alan.guerrao@hotmail.com>
2025-03-06 23:00:19 -0600

    Estilizacion de las cards

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,12 +1,11 @@
 import { useNavigate } from "react-router-dom";
+import './TaskCard.css'; // Importar los estilos de la card
 
-export function TaskCard({task}) {
+export function TaskCard({ task }) {
     const navigate = useNavigate();
 
     return (
-        <div style={{border: '1px solid black', padding: '10px', margin: '10px', backgroundColor: '#181818', cursor: 'pointer', color: 'white'}}
-        onClick={() => navigate(`/tasks/${task.id}`)}
-        >
+        <div className="card-container" onClick={() => navigate(`/tasks/${task.id}`)}>
             <h2>{task.title}</h2>
             <p>{task.description}</p>
             <hr />
--- client/src/components/TasksList.jsx
+++ client/src/components/TasksList.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react"
 import { getAllTasks } from "../api/tasks.api"
 import { TaskCard } from "./TaskCard"
+import './TasksList.css'; // Importar los estilos de la lista
 
 export function TasksList() {
     const [ tasks, setTasks ] = useState([])
 
-    useEffect(()=>{
-        async function loadTasks(){
+    useEffect(() => {
+        async function loadTasks() {
             const res = await getAllTasks()
             setTasks(res.data)
         }
         loadTasks()
-    },[])
+    }, [])
 
     return (
-        <div>
+        <div className="tasks-list">
             {tasks.map(task => (
                 <TaskCard key={task.id} task={task} />
             ))}
