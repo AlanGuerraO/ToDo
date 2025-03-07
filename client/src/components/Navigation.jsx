040eb72280dff7aeb1e26a77025b9f7b774ba2c6
Alan Guerra <alan.guerrao@hotmail.com>
2025-03-06 22:15:15 -0600

    Estilizacion del header, y el fondo principal de la pagina, añadiendo el logo de la aplicacion y el nombre de la misma.

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import './Navigation.css';
 
 export function Navigation() {
     return (
         <header>
             <nav>
                 <Link to="/">
-                    <h1>Tasks App</h1>
+                    <h1 className="container_logo">
+                        <img src="/logo.png" alt="Logo de la apliacion" className="logo"/>
+                        To-Do App
+                    </h1>
                 </Link>
                 <ul>
                     <li>
                         <Link to='/tasks'>Inicio</Link>
                     </li>
                     <li>
-                        <Link to='/tasks-create'>Create Task</Link>
+                        <Link to='/tasks-create' className="crear">Create Task</Link>
                     </li>
                 </ul>
             </nav>
